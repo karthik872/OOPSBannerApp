c4999a1b0c0eb34777ecb0580300582960a373ad
Karthikeyan <karthikeyanbalamurugan50@gmail.com>
2026-02-28 22:24:55 +0530

    feature/UC3: Refactor banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,45 +1,55 @@
-/**
- * OOPSBannerApp
- * UC2: Render OOPS as Banner using Print Statements
- * 
- * @author Karthik
- * @version 2.0
- */
-
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String O1L1 = " ******** ";
-        String O1L2 = "*        *";
-        String O1L3 = "*        *";
-        String O1L4 = "*        *";
-        String O1L5 = "*        *";
-        String O1L6 = "*        *";
-        String O1L7 = " ******** ";
+        String line1 = String.join(" ",
+                " ******** ",
+                " ******** ",
+                " ******** ",
+                " ******** ");
 
-        String P1L1 = " ******** ";
-        String P1L2 = "*        *";
-        String P1L3 = "*        *";
-        String P1L4 = " ******** ";
-        String P1L5 = "*         ";
-        String P1L6 = "*         ";
-        String P1L7 = "*         ";
+        String line2 = String.join(" ",
+                "*        *",
+                "*        *",
+                "*        *",
+                "*        *");
 
-        String S1L1 = " ******** ";
-        String S1L2 = "*         ";
-        String S1L3 = "*         ";
-        String S1L4 = " ******** ";
-        String S1L5 = "         *";
-        String S1L6 = "         *";
-        String S1L7 = " ******** ";
+        String line3 = String.join(" ",
+                "*        *",
+                "*        *",
+                "*        *",
+                "*        *");
 
-        System.out.println(O1L1 + " " + O1L1 + " " + P1L1 + " " + S1L1);
-        System.out.println(O1L2 + " " + O1L2 + " " + P1L2 + " " + S1L2);
-        System.out.println(O1L3 + " " + O1L3 + " " + P1L3 + " " + S1L3);
-        System.out.println(O1L4 + " " + O1L4 + " " + P1L4 + " " + S1L4);
-        System.out.println(O1L5 + " " + O1L5 + " " + P1L5 + " " + S1L5);
-        System.out.println(O1L6 + " " + O1L6 + " " + P1L6 + " " + S1L6);
-        System.out.println(O1L7 + " " + O1L7 + " " + P1L7 + " " + S1L7);
+        String line4 = String.join(" ",
+                "*        *",
+                "*        *",
+                " ******** ",
+                " ******** ");
+
+        String line5 = String.join(" ",
+                "*        *",
+                "*        *",
+                "*         ",
+                "*        ");
+
+        String line6 = String.join(" ",
+                "*        *",
+                "*        *",
+                "*         ",
+                "*        ");
+
+        String line7 = String.join(" ",
+                " ******** ",
+                " ******** ",
+                "*         ",
+                " ******** ");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
     }
 }
