ded145890ca68012f419e2e5bda5a35ac3332717
Karthikeyan <karthikeyanbalamurugan50@gmail.com>
2026-02-28 17:15:03 +0530

    feature/UC2: Render OOPS banner using print statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,45 @@
+/**
+ * OOPSBannerApp
+ * UC2: Render OOPS as Banner using Print Statements
+ * 
+ * @author Karthik
+ * @version 2.0
+ */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
-    }
 
+        String O1L1 = " ******** ";
+        String O1L2 = "*        *";
+        String O1L3 = "*        *";
+        String O1L4 = "*        *";
+        String O1L5 = "*        *";
+        String O1L6 = "*        *";
+        String O1L7 = " ******** ";
+
+        String P1L1 = " ******** ";
+        String P1L2 = "*        *";
+        String P1L3 = "*        *";
+        String P1L4 = " ******** ";
+        String P1L5 = "*         ";
+        String P1L6 = "*         ";
+        String P1L7 = "*         ";
+
+        String S1L1 = " ******** ";
+        String S1L2 = "*         ";
+        String S1L3 = "*         ";
+        String S1L4 = " ******** ";
+        String S1L5 = "         *";
+        String S1L6 = "         *";
+        String S1L7 = " ******** ";
+
+        System.out.println(O1L1 + " " + O1L1 + " " + P1L1 + " " + S1L1);
+        System.out.println(O1L2 + " " + O1L2 + " " + P1L2 + " " + S1L2);
+        System.out.println(O1L3 + " " + O1L3 + " " + P1L3 + " " + S1L3);
+        System.out.println(O1L4 + " " + O1L4 + " " + P1L4 + " " + S1L4);
+        System.out.println(O1L5 + " " + O1L5 + " " + P1L5 + " " + S1L5);
+        System.out.println(O1L6 + " " + O1L6 + " " + P1L6 + " " + S1L6);
+        System.out.println(O1L7 + " " + O1L7 + " " + P1L7 + " " + S1L7);
+    }
 }
